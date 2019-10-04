d117bc805ee336889d6ab1c15690a7b05ef0fc43
digitalmoleman <digitalmoleguy@gmail.com>
2019-10-04 15:28:17 +0200

    merged place and remove tools.

--- js/tools.js
+++ js/tools.js
@@ -1,14 +1,29 @@
+class Tile{
+    constructor(x, y, sprite){
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+    }
+}
+
+class Block extends Tile{
+    constructor(x, y){
+        super(x, y, sprites.tiles.block);
+    }
+}
 const tools = {
     TilePlacer: new class {
         constructor() {
             this.mouseIsDown = false;
+            this.mouseButton = undefined;
             this.toolbar = [
 
             ]
         }
-        mouseDown() {
+        mouseDown(e) {
             this.mouseIsDown = true;
-            this.addTile();
+            this.mouseButton = e.button;
+            (this.mouseButton == 0) ? addTile() : removeTile();
         }
 
         mouseUp() {
@@ -17,44 +32,27 @@ const tools = {
 
         mouseMove() {
             if (this.mouseIsDown) {
-                this.addTile();
+                (this.mouseButton == 0) ? addTile() : removeTile();
             }
         }
 
-        addTile() {
-            var onTile = Editor.activeLevel.tiles.filter((tile) => (tile.x == Editor.cursorX() + scrollX && tile.y == Editor.cursorY() + scrollY));
-            if (onTile.length == 0) {
-
-                Editor.activeLevel.tiles.push({
-                    type: tileSelect.value,
-                    x: Editor.cursorX() + scrollX,
-                    y: Editor.cursorY() + scrollY
-                });
-            }
-        }
-    },
-    TileRemover: new class {
-        constructor() {
-            this.mouseIsDown = false;
-        }
-        mouseDown() {
-            this.mouseIsDown = true;
-            if (Editor.activeLevel.tiles.filter((tile) => (tile.x == Editor.cursorX() && tile.y == Editor.cursorY())).length !== 0) this.removeTile();
-        }
-
-        mouseUp() {
-            this.mouseIsDown = false;
-        }
-
-        mouseMove() {
-            if (this.mouseIsDown) {
-                if (Editor.activeLevel.tiles.filter((tile) => (tile.x == Editor.cursorX() && tile.y == Editor.cursorY())).length !== 0) this.removeTile();
-            }
-        }
-
-        removeTile() {
-            var tileIndex = Editor.activeLevel.tiles.findIndex((tile) => (tile.x == Editor.cursorX() && tile.y == Editor.cursorY()));
-            Editor.activeLevel.tiles.splice(tileIndex, 1);
-        }
+        
     }
+}
+
+addTile = () => {
+    var onTile = Editor.activeLevel.tiles.filter((tile) => (tile.x == Editor.cursorX() + scrollX && tile.y == Editor.cursorY() + scrollY));
+    if (onTile.length == 0) {
+
+        Editor.activeLevel.tiles.push({
+            type: tileSelect.value,
+            x: Editor.cursorX() + scrollX,
+            y: Editor.cursorY() + scrollY
+        });
+    }
+}
+
+removeTile = () => {
+    var tileIndex = Editor.activeLevel.tiles.findIndex((tile) => (tile.x == Editor.cursorX() && tile.y == Editor.cursorY()));
+if(tileIndex >= 0)   Editor.activeLevel.tiles.splice(tileIndex, 1);
 }
