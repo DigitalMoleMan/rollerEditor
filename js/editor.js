f0b5d1bdd70c5ebbe08df1cc743eb318e5452d3e
digitalmoleman <digitalmoleguy@gmail.com>
2019-10-04 15:26:47 +0200

    cleanup, structuring, commenting

--- js/editor.js
+++ js/editor.js
@@ -1,50 +1,60 @@
-Editor = new class {
+class Editor {
     constructor() {
         this.activeTool = () => tools[toolSelect.value];
-        this.activeLevel = new Level({
-            name: level[0].name,
-            tiles: new Array,
-            doors: new Array
-        });
+        this.activeLevel = new Level();
 
         this.cursorX = () => Math.floor(mouse.x / 16);
         this.cursorY = () => Math.floor(mouse.y / 16);
 
-        canvas.addEventListener("mousedown", () => this.activeTool().mouseDown());
-        canvas.addEventListener("mouseup", () => this.activeTool().mouseUp());
-        canvas.addEventListener("mousemove", () => this.activeTool().mouseMove());
+        canvas.addEventListener("mousedown", (e) => this.activeTool().mouseDown(e));
+        canvas.addEventListener("mouseup", (e) => this.activeTool().mouseUp(e));
+        canvas.addEventListener("mousemove", (e) => this.activeTool().mouseMove(e));
+        canvas.addEventListener("wheel", (e) => this.activeTool().wheel(e));
     }
 
     newLevel() {
-        this.activeLevel = new Level();
-        scrollX = 0;
-        scrollY = 0;
-    }
-
-    openLevel(e) {
-        var input = e.target;
-
-        var reader = new FileReader();
-        reader.onload = () => {
-            this.activeLevel = new Level(JSON.parse(reader.result));
+        if (confirm("Are you sure?")) {
+            this.activeLevel = new Level();
+            scrollX = 0;
+            scrollY = 0;
         }
-        reader.readAsText(input.files[0]);
     }
 
-    saveLevel() {
-        var levelString = JSON.stringify(this.activeLevel);
-        console.log(levelString);
-        /*
-        var exportData = 'data:text/json;charset=utf-8,';
-        exportData += escape(levelString);
-        var encodedUri = encodeURI(exportData);
-        window.open(encodedUri);
-        */
+    /**
+     * 
+     * @param {File} inputFile 
+     */
+    async setActiveLevelFromFile(inputFile) {
+        var levelData = await this.getLevelDataFromFile(inputFile);
+        this.activeLevel = new Level(levelData);
+    }
 
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(levelString);
-        var dlAnchorElem = document.createElement('a');
+    /**
+     * @param {File} inputFile JSON.
+     * @returns {Object} level data as a json object.
+     */
+    getLevelDataFromFile = inputFile => {
+        return new Promise(resolve => {
+            var reader = new FileReader();
+            reader.onload = () => resolve(JSON.parse(reader.result));
+            reader.readAsText(inputFile);
+        });
+    };
+
+    /**
+     * 
+     * @param {Level} level 
+     */
+    saveLevel(level) {
+        var levelString = JSON.stringify(level);
+        console.log(levelString);
+        var dataStr =
+            "data:text/json;charset=utf-8," + encodeURIComponent(levelString);
+        var dlAnchorElem = document.createElement("a");
         dlAnchorElem.setAttribute("href", dataStr);
         dlAnchorElem.setAttribute("download", "level.json");
         dlAnchorElem.click();
     }
-}
+};
+
+Editor = new Editor();
